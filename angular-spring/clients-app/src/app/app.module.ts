5c250b617ae4e1f1162325cbee428b41614f58a4
Abel Fernandez <abelfergodevelop@gmail.com>
2020-02-17 14:38:21 +0100

    Seccion 1 - Video 11 - Creando nuevo componente FooterComponent

--- angular-spring/clients-app/src/app/app.module.ts
+++ angular-spring/clients-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
